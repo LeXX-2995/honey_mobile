3a0a7765ac6bc317d6e577aaccb90524596c2676
Timur Degtyaryov <timur.degtiarev@hotmail.com>
2022-01-12 02:54:07 +0500

    Updated Xamarin.Forms

---
+++ BarcodeReaderSample/BarcodeReaderSample.Droid/obj/Release/110/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,45 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "BarcodeReaderSample.Droid.dll" comes first in this list. */
+		"BarcodeReaderSample.Droid.dll",
+		"Xamarin.AndroidX.AppCompat.AppCompatResources.dll",
+		"Xamarin.AndroidX.AppCompat.dll",
+		"Xamarin.AndroidX.CardView.dll",
+		"Xamarin.AndroidX.CoordinatorLayout.dll",
+		"Xamarin.AndroidX.Core.dll",
+		"Xamarin.AndroidX.CustomView.dll",
+		"Xamarin.AndroidX.DrawerLayout.dll",
+		"Xamarin.AndroidX.Fragment.dll",
+		"Xamarin.AndroidX.Legacy.Support.Core.UI.dll",
+		"Xamarin.AndroidX.Lifecycle.Common.dll",
+		"Xamarin.AndroidX.Lifecycle.LiveData.Core.dll",
+		"Xamarin.AndroidX.Lifecycle.ViewModel.dll",
+		"Xamarin.AndroidX.Loader.dll",
+		"Xamarin.AndroidX.RecyclerView.dll",
+		"Xamarin.AndroidX.SwipeRefreshLayout.dll",
+		"Xamarin.AndroidX.ViewPager.dll",
+		"Xamarin.Google.Android.Material.dll",
+		"DataCollectionLib.dll",
+		"FormsViewGroup.dll",
+		"FreshIOC.dll",
+		"FreshMvvm.dll",
+		"Honeywell.AIDC.CrossPlatform.BarcodeReader.dll",
+		"Newtonsoft.Json.dll",
+		"RestSharp.dll",
+		"RestSharp.Serializers.NewtonsoftJson.dll",
+		"Xamarin.AndroidX.Activity.dll",
+		"Xamarin.AndroidX.SavedState.dll",
+		"Xamarin.Essentials.dll",
+		"Xamarin.Forms.Core.dll",
+		"Xamarin.Forms.Platform.Android.dll",
+		"Xamarin.Forms.Platform.dll",
+		"Xamarin.Forms.Xaml.dll",
+		"Xamarin.Google.Guava.ListenableFuture.dll",
+		"Xamarin.Jetbrains.Annotations.dll",
+		"Xamarin.Kotlin.StdLib.Common.dll",
+		"Xamarin.Kotlin.StdLib.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+}
